e6bf59ccc609b2c8e8ea70aa8524e72b9993de7a
Shahiduzzaman Robin <mds.zamanrobin@gmail.com>
2026-03-22 15:20:03 +0600

    Cleanup: Remove unused scripts and temporary debug routes

--- check_admins.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-async function main() {
-  const admins = await prisma.admins.findMany()
-  console.log(admins)
-}
-main()
